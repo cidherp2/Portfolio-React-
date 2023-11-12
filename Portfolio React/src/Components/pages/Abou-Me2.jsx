4683d21cba09b91f1c53c31f5751e9725c46b0af
Alejandro Larios <alexlarios70@hotmail.com>
2023-11-12 05:04:04 -0600

    Carousel now working properly, fixes a bug that caused the first image to load incorrectly

--- Portfolio React/src/Components/pages/Abou-Me2.jsx
+++ Portfolio React/src/Components/pages/Abou-Me2.jsx
@@ -1,46 +1,58 @@
-import Carousel from 'react-material-ui-carousel'
+import { useState } from "react";
 import React from 'react';
-import img1 from "../../assets/Larios-2.jpg"
-import img2 from "../../assets/Larios-21.jpg"
-import img3 from "../../assets/Larios-4.jpg"
-
-const Item = ({ item }) => (
-  <div >
-    <img id = "proyect-Center" className= "myImage" src = {item.imageUrl} alt={item.name}/>
-    <h2>{item.name}</h2>
-    <p>{item.description}</p>
-  </div>
-);
+import img1 from "../../assets/proyect 1.png"
+import img2 from "../../assets/proyect2.png"
+import img3 from "../../assets/proyect3.png"
+import Carousel from 'react-material-ui-carousel'
 
 export default function AboutMe(props) {
 
   var items = [
     {
-        name: "Random Name #1",
-        description: "Probably the most random thing you have ever seen!",
+        name: "Movie night planner",
+        description: "Plan your nights in a fun way!",
         imageUrl: img1
     },
     {
-        name: "Random Name #2",
-        description: "Hello World!",
+        name: "Travelifico",
+        description: "Traveling abroad? fancy a bite of something delicious? Travelifico is for you!",
         imageUrl: img2
     },
     {
-        name: "Random Name #2",
-        description: "Hello World!",
+        name: "Wheather dashboard",
+        description: "Never get caught of guard by the weather again!",
         imageUrl: img3
     }
 ]
+const [firstImgLoaded, setFirstImgLoaded] = useState(false);
+
     return (
       <>
        <div className="proyects">
         <h1 className="portfolioTitle">My Projects</h1>
        </div>
-       <Carousel >
+       <img
+       src = {img1}
+       alt="movi night"
+       onLoad={() => setFirstImgLoaded(true)}
+        style={{ display: "none" }}
+        />
+       {firstImgLoaded && (
+       <Carousel autoPlay={false}>
             {
                 items.map( (item, i) => <Item key={i} item={item} /> )
             }
         </Carousel>
+       )}
       </>
     )
   }
+
+  const Item = ({ item }) => (
+    <div id ="proyectsConti">
+      <img id ="proyect-Center"src={item.imageUrl} alt={item.name} className='myImage2' />
+      <h2>{item.name}</h2>
+      <p>{item.description}</p>
+    </div>
+    
+  );
